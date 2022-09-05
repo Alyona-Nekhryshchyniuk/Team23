5888829c0611c3b1fc287f9edade8a5ec855f0bb
supirgigi <107944673+supirgigi@users.noreply.github.com>
2022-09-05 18:36:33 +0300

    mobile menu script changes

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,13 +3,17 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    menulinks: document.querySelectorAll('.mobile-menu-nav__link'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.menulinks.forEach(menulink => {
+    menulink.addEventListener('click', toggleModal);
+  });
 
   function toggleModal() {
+    document.body.classList.toggle('locked');
     refs.menu.classList.toggle('is-open');
   }
 })();
-
