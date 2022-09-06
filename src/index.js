10fdc75c2c8ae1b4939d1ba7506822da1f27ebde
supirgigi <107944673+supirgigi@users.noreply.github.com>
2022-09-06 23:26:12 +0300

    modal fixes

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import './js/gallery';
 import './js/review-slider';
 import './js/mobile-menu';
 import './js/arrow-toggle';
+import './js/modal';
+import './js/modal_buy';
+import './js/modal_ingridient';
+import './js/modal_location';
+import './js/modal_made';
